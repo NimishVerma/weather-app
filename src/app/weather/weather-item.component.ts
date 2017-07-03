bd73b730b78d7a36385be582f2f5c7be2421ceb7
bigpythonimish <nimishverma@ymail.com>
2017-07-03 10:55:05 +0530

    Added css

--- src/app/weather/weather-item.component.ts
+++ src/app/weather/weather-item.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
-
+import {WeatherItem} from "./weather-item";
 @Component({
   selector: 'weather-item',
   templateUrl: './weather-item.component.html',
+  styleUrls: ['./weather-item.css']
 })
-export class WeatherItemComponent{ }
+export class WeatherItemComponent{
+          weatherItem: WeatherItem;
+          
+  }
--- src/app/weather/weather-list.component.ts
+++ src/app/weather/weather-list.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 
 import { WeatherItemComponent } from './weather-item.component';
-
-
-
+import { WeatherItem } from './weather-item';
+import { WEATHER_ITEMS} from './weather.data';
 
 @Component({
   selector: 'weather-list',
-  template: `<section class="weather-list">HIHI
-  <weather-item></weather-item>
+  template: `<section class="weather-list">
+  <weather-item *ngFor="#weatherItem of weatherItems"></weather-item>
   </section>`,
 
 })
 
 export class WeatherListComponent{
+  weatherItems: WeatherItem[];
+  ngOnit():any{
+    this.weatherItems = WEATHER_ITEMS;
+  }
 
 }
